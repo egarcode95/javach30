5ded69c786b7b7f920195a7ec97e9a78de2873ef
egarcode95 <egarcode95@gmail.com>
2023-09-26 15:51:27 -0600

    adding files

--- gs-accessing-data-jpa-main/complete/src/main/java/com/example/accessingdatajpa/controller/CustomerController.java
+++ gs-accessing-data-jpa-main/complete/src/main/java/com/example/accessingdatajpa/controller/CustomerController.java
@@ -1,22 +1,17 @@
 package com.example.accessingdatajpa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.accessingdatajpa.entity.Customer;
 import com.example.accessingdatajpa.repository.CustomerRepository;
 
 import lombok.extern.log4j.Log4j2;
 
+@CrossOrigin(origins = "*")
 @RestController // @Controller @ResponseBody
 @RequestMapping("api/v1/customers") // localhost:8080/api/vi/customers
 @Log4j2
